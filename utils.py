0b596c85b11499eb97df9ab7b16cc3c0604fa470
David Bell <davidgrebell@gmail.com>
2017-08-24 10:42:28 -0700

    changes to fix some bugs

--- utils.py
+++ utils.py
@@ -48,8 +48,8 @@ def filter_by_ext(l, ext):
 		if i.endswith(ext):
 			yield i
 
-def all_files_with_ext(path_to_dir, ext):
-	path_to_dir = file_path(__file__, path_to_dir)
+def all_files_with_ext(curr_file, path_to_dir, ext):
+	path_to_dir = file_path(curr_file, path_to_dir)
 	all_files = os.listdir(path_to_dir)
 	return [f for f in filter_by_ext(all_files, ext)]
 
@@ -63,17 +63,17 @@ def normalize_matrix(mat, norming_factor):
 		for j, item in enumerate(l):
 			mat[i][j] /= norming_factor
 
-def read_matrix_file(path, name):
-	return read_csv(__file__, os.path.join(path, name), preprocess=preprocess)
+def read_matrix_file(curr_file, path, name):
+	return read_csv(curr_file, os.path.join(path, name), preprocess=preprocess)
 
-def average_files(path_to_dir, ext):
-	files = all_files_with_ext(path_to_dir, ext)
+def average_files(curr_file, path_to_dir, ext):
+	files = all_files_with_ext(curr_file, path_to_dir, ext)
 	if len(files) < 1:
 		raise 'There should be more than 0 files'
 	print('FILE: %s' % files[0])
-	initial = read_matrix_file(path_to_dir, files[0])
+	initial = read_matrix_file(curr_file, path_to_dir, files[0])
 	for i in range(1, len(files)):
 		print('FILE: %s' % files[i])
-		add_matrix(initial, read_matrix_file(path_to_dir, files[i]))
+		add_matrix(initial, read_matrix_file(curr_file, path_to_dir, files[i]))
 	normalize_matrix(initial, len(files))
 	return initial
