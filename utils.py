364dd8b8eda42f394739b05451ab0c4a10a153df
David Bell <davidgrebell@gmail.com>
2017-08-24 10:34:49 -0700

    add matrix averaging utilities

--- utils.py
+++ utils.py
@@ -42,3 +42,38 @@ def write_csv(curr_file, rel_path, delimiter=',', to_dump=None):
 		for row in to_dump:
 			csv_writer.writerow(row)
 	data_file.close()
+
+def filter_by_ext(l, ext):
+	for i in l:
+		if i.endswith(ext):
+			yield i
+
+def all_files_with_ext(path_to_dir, ext):
+	path_to_dir = file_path(__file__, path_to_dir)
+	all_files = os.listdir(path_to_dir)
+	return [f for f in filter_by_ext(all_files, ext)]
+
+def add_matrix(dest, mat):
+	for i, l in enumerate(mat):
+		for j, item in enumerate(l):
+			dest[i][j] += item
+
+def normalize_matrix(mat, norming_factor):
+	for i, l in enumerate(mat):
+		for j, item in enumerate(l):
+			mat[i][j] /= norming_factor
+
+def read_matrix_file(path, name):
+	return read_csv(__file__, os.path.join(path, name), preprocess=preprocess)
+
+def average_files(path_to_dir, ext):
+	files = all_files_with_ext(path_to_dir, ext)
+	if len(files) < 1:
+		raise 'There should be more than 0 files'
+	print('FILE: %s' % files[0])
+	initial = read_matrix_file(path_to_dir, files[0])
+	for i in range(1, len(files)):
+		print('FILE: %s' % files[i])
+		add_matrix(initial, read_matrix_file(path_to_dir, files[i]))
+	normalize_matrix(initial, len(files))
+	return initial
