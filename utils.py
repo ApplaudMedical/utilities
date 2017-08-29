ed4f11674d3a0d9e567f4d770cdc6dcb756b94e5
David Bell <davidgrebell@gmail.com>
2017-08-29 13:05:03 -0700

    with comments

--- utils.py
+++ utils.py
@@ -62,11 +62,12 @@ def filter_by_name_frags(name, name_frags):
 		except ValueError:
 			break
 
+# filters list of strings, leaving on elements that contain name frags, in order
+# NOTE: uses first instance of name fragment and no other instance
 def filter_list_by_name_frags(l, name_frags):
 	for name in l:
 		for matching_name in filter_by_name_frags(name, name_frags):
 			yield matching_name
-
 
 def all_in_dir(curr_file, path_to_dir):
 	path_to_dir = file_path(curr_file, path_to_dir)
@@ -82,10 +83,12 @@ def all_dirs_from_dir(curr_file, path_to_dir):
 	all_entries = all_in_dir(curr_file, path_to_dir)
 	return [name for name in all_entries if os.path.isdir(os.path.join(dir_path, name))]
 
+# finds all files in root direct 'path_to_dir' that contain name fragments in order
 def all_files_with_name_frags(curr_file, path_to_dir, name_frags):
 	all_files = all_files_from_dir(curr_file, path_to_dir)
 	return [f for f in filter_list_by_name_frags(all_files, name_frags)]
 
+# finds all directories in root direct 'path_to_dir' that contain name fragments in order
 def all_dirs_with_name_frags(curr_file, path_to_dir, name_frags):
 	all_dirs = all_dirs_from_dir(curr_file, path_to_dir)
 	return [d for d in filter_list_by_name_frags(all_dirs, name_frags)]
@@ -100,9 +103,15 @@ def normalize_matrix(mat, norming_factor):
 		for j, item in enumerate(l):
 			mat[i][j] /= norming_factor
 
-def read_matrix_file(curr_file, path, name=''):
-	return read_csv(curr_file, os.path.join(path, name), preprocess=preprocess)
+def read_matrix_file(curr_file, path, name=None):
+	path = os.path.join(path, name) if name is not None else path
+	return read_csv(curr_file, path, preprocess=preprocess)
 
+# assumes all files are csvs that can be read using 'read_matrix_file'
+# if 'files' is an array of strings, filters list for names that contain name fragments
+# then computes averages from each value in the matrix across all filtered files
+# returns (averaged arrays, files that were filtered out)
+# if 'files' is None, does the same operation, but 'files' becomes all files found in 'path_to_dir'
 def average_files(curr_file, path_to_dir, name_frags=[], files=None, print_on=False):
 	if files is None:
 		files = all_files_with_name_frags(curr_file, path_to_dir, name_frags)
@@ -130,19 +139,27 @@ def batch_average_files(curr_file, path_to_dir, name_frag_sets):
 def reduce_mult(l):
 	return functools.reduce(lambda e1, e2: e1 * e2, l, 1)
 
-''' specifically designed for matplotlib, so this generates a separate list of coordinates
-	for each dimenion '''
-def create_points_for_domain(domain, inclusive=False):
-	if inclusive:
-		domain = map_to_list(lambda d: d+1, domain)
+# effectively the same as cartesian, but only provide the magnitude of each array
+# given [1, 2, 1] generates
+# [ [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+#	[0, 0, 1, 1, 2, 2, 0, 0, 1, 1, 2, 2],
+#	[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1] ] 
+def create_points_for_domain(domain):
 	ranges = map_to_list(lambda dim: [i for i in range(0, dim)], domain)
+	return cartesian(ranges)
+
+# multidimensional generalization of a cartesian proces
+# given [2, 4, 6] and [2, 5, 8, 9] generates
+# [[2, 2, 2, 2, 4, 4, 4, 4, 6, 6, 6, 6], [2, 5, 8, 9, 2, 5, 8, 9, 2, 5, 8, 9]]
+def cartesian(*arrs):
+	domain = map_to_list(lambda a: len(a), arrs)
 	coordinate_lists = []
 	for i, dim in enumerate(domain):
 		coords = []
 		mult = 1
 		if i != len(domain) - 1:
 			mult = reduce_mult(domain[i+1:])
-		for e in ranges[i]:
+		for e in arrs[i]:
 			coords += (mult * [e])
 		repeat_factor = reduce_mult(domain[0:i])
 		if repeat_factor > 0:
