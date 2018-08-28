67d192b9c821ab56865c98e425a90bd97269580d
David Bell <davidgrebell@gmail.com>
2018-08-27 17:24:16 -0700

    fix lines

--- graph.py
+++ graph.py
@@ -9,17 +9,17 @@ class GraphManager:
 	def draw_refs(self, horizontal=True, vertical=True):
 		if horizontal:
 			y = self.ax.get_yaxis()
-			self.draw_ref_lines(y.get_ticklocs()[1:-1])
+			self.draw_ref_lines(y.get_ticklocs())
 		if vertical:
 			x = self.ax.get_xaxis()
-			self.draw_ref_lines(x.get_ticklocs()[1:-1], vertical=True)
+			self.draw_ref_lines(x.get_ticklocs(), vertical=True)
 
 	def draw_ref_lines(self, coords, vertical=False):
 		if vertical:
 			lims = self.ax.get_ylim()
 		else:
 			lims = self.ax.get_xlim()
-		points = np.arange(lims[0], lims[1])
+		points = np.arange(lims[0], lims[1], np.abs(lims[1] - lims[0]) / 100)
 
 		for coord in coords:
 			if vertical:
