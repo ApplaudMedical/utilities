226b399ea938d0d282dea41ab99bc806069e54ce
David Bell <davidgrebell@gmail.com>
2019-04-03 10:53:27 -0700

    add SmartGraph

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import numpy as np
+from utils import select, to_unique_vals
 
 class GraphManager:
 	def __init__(self, fig, ax):
@@ -53,6 +54,49 @@ def gen_plot(fig=None, pos=111):
 def scatter(ax, x, xerr, y, yerr, err_bar_thickness=0.5, color='black', err_bar_color='black', label='', s=3, marker=None, facecolors=None):
 	ax.scatter(x, y, color=color, label=label, s=s, marker=marker, facecolors=color if facecolors is None else facecolors)
 	ax.errorbar(x, y, xerr=xerr, yerr=yerr, fmt='none', color=err_bar_color, capsize=3, elinewidth=err_bar_thickness, capthick=err_bar_thickness)
+
+class SmartGraph:
+	def __init__(self, fig, ax, data):
+		self.fig = fig
+		self.ax = ax
+		self.data = data
+
+	def run_statistics(self, x_axis_col, y_axis_col):
+		processed_data = []
+		for group in self.data:
+			unique_vals, = to_unique_vals(group, [x_axis_col])
+			means = []
+			cis = []
+			for x_val in unique_vals:
+				y_vals = select(group, {x_axis_col: x_vall})[y_axis_col]
+				means.append(y_vals.mean())
+				cis.append(1.96 * y_vals.std() / np.sqrt(len(y_vals)))
+			processed_data.append({'x_vals': unique_vals, 'y_vals': means, 'y_cis': cis})
+	    return processed_data
+
+	def check_setting(self, setting, required_len, default):
+		if setting is None:
+			return [setting] * required_len
+		elif len(setting) != required_len:
+			raise ValueError("Setting must be the 'required_len' %d" % required_len)
+		else:
+			return setting
+
+	def plot(self, mode='scatter', x_axis_col, y_axis_col, err_bar_thickness=0.5, colors=None, err_bar_colors=None, labels=None, s=3, markers=None, facecolors=None):
+		if mode in ['scatter', 'statistical']:
+			self.ax.clear()
+			colors = self.check_setting(colors, len(self.data), 'black')
+			labels = self.check_setting(labels, len(self.data), '')
+			markers = self.check_setting(markers, len(self.data), None)
+
+			if mode == 'scatter':
+				for i, group in enumerate(self.data):
+					ax.scatter(group[x_axis_col], group[y_axis_col], c=colors[i], labels=labels[i], marker=markers[i], s=s, facecolors=facecolors)
+			else:
+				err_bar_colors = self.check_setting(err_bar_colors, len(self.data), 'black')
+				processed_data = self.run_statistics(x_axis_col, y_axis_col)
+				for i, group in enumerate(processed_data):
+					scatter(ax, group['x_vals'], None, group['y_vals'], group['y_cis'], err_bar_thickness=err_bar_thickness, color=colors[i], label=labels[i], marker=markers[i], s=s, facecolors=facecolors)
 
 # tableau20 colors borrowed from http://www.randalolson.com/2014/06/28/how-to-make-beautiful-data-visualizations-in-python-with-matplotlib/
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
